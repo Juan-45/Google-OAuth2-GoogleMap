f3ffbfeb84a6c8c8ce74552cb0931416231cbba6
Juan-45 <quijuan12@gmail.com>
2021-12-15 16:40:51 -0300

    Implement useGoogleMaps

--- src/mapContainer/Map.js
+++ src/mapContainer/Map.js
@@ -1,43 +1,7 @@
-import { useRef, useEffect, useState } from "react";
+import useGoogleMaps from "hooks/useGoogleMaps";
 
 const Map = ({ manageMap }) => {
-  const { markersCoordinates, iconURL, center, zoom } = manageMap;
-  const [mapInstance, setMapInstance] = useState();
-  const ref = useRef();
-
-  useEffect(() => {
-    const initialiceMap = async () => {
-      const map = await new window.google.maps.Map(ref.current, {
-        center,
-        zoom,
-      });
-      return map;
-    };
-
-    if (ref.current && !mapInstance) {
-      initialiceMap().then((map) => setMapInstance(map));
-    }
-  }, [center, zoom, mapInstance]);
-
-  useEffect(() => {
-    if (ref.current && mapInstance && markersCoordinates) {
-      const iconBaseURL = "http://maps.google.com/mapfiles/ms/icons/";
-
-      const icons = {
-        blueBubble: iconBaseURL + "blue-dot.png",
-      };
-
-      const markerConfig = markersCoordinates.map((item) => ({
-        position: { ...item },
-        icon: iconURL ? iconURL : icons.blueBubble,
-        map: mapInstance,
-      }));
-
-      for (let i = 0; i < markerConfig.length; i++) {
-        new window.google.maps.Marker(markerConfig[i]);
-      }
-    }
-  }, [mapInstance, iconURL, markersCoordinates]);
+  const { ref } = useGoogleMaps({ manageMap });
 
   return (
     <div id="mapContainer">
