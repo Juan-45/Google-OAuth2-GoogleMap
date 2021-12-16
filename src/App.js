0b3dbab51a4d76f99fa6daf7c05d57d8f0ab70b6
Juan-45 <quijuan12@gmail.com>
2021-12-16 15:57:45 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     handleAuthButton,
     revokeAccess,
     isUserLogged,
-    currentUser,
+    basicProfile,
   } = useOAuth2({
     CLIENT_KEY: process.env.REACT_APP_GOOGLE_DRIVE_CLIENT_ID,
     API_KEY: process.env.REACT_APP_GOOGLE_DRIVE_API_KEY,
@@ -19,32 +19,55 @@ function App() {
       "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest",
   });
 
-  const markersCoordinates = [
-    { lat: -33.91721, lng: 151.2263 },
-    { lat: -33.91539, lng: 151.2282 },
-    { lat: -33.91747, lng: 151.22912 },
-    { lat: -33.91727341958453, lng: 151.23348314155578 },
+  const markersSettings = [
+    {
+      position: { lat: -32.950849, lng: -60.676095 },
+      adress: "Av. Pellegrini 3676",
+      title: "Rosario 1",
+      contact: "+54-9-341-5645782",
+    },
+    {
+      position: { lat: -32.948202, lng: -60.654138 },
+      adress: "Bv. Oroño 1157",
+      title: "Rosario 2",
+      contact: "+54-9-341-1457412",
+    },
+    {
+      position: { lat: -32.966602, lng: -60.647773 },
+      adress: "Blvd. 27 de Febrero 1400",
+      title: "Rosario 3",
+      contact: "+54-9-341-4678275",
+    },
+    {
+      position: { lat: -32.96949, lng: -60.667983 },
+      adress: "Av. Ovidio Lagos 3126",
+      title: "Rosario 4",
+      contact: "+54-9-341-8655982",
+    },
   ];
 
   return (
     <div className="App">
       <Authentication
-        manageAuth={{
+        authSettings={{
           handleAuthButton,
           isUserLogged,
           revokeAccess,
           manageRequest,
-          currentUser,
+          basicProfile,
         }}
       />
-
       {isUserLogged ? (
         <Map
-          manageMap={{
+          mapSettings={{
             apiKey: process.env.REACT_APP_GOOGLE_DRIVE_API_KEY,
-            markersCoordinates,
-            center: { lat: -33.91721, lng: 151.2263 },
-            zoom: 15,
+            markersSettings,
+            center: { lat: -32.959256, lng: -60.660423 },
+            zoom: 14,
+            iconSetting: {
+              url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png",
+              scaledSize: { height: 45, width: 45 },
+            },
           }}
         />
       ) : null}
