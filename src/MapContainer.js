37853835372996928571062a2fe36455988e05d8
Juan-45 <quijuan12@gmail.com>
2021-12-17 15:46:49 -0300

    Using status to manage error & loading

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,14 +1,23 @@
-import { Wrapper } from "@googlemaps/react-wrapper";
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
 import Map from "mapContainer/Map";
 
 const MapContainer = ({ mapInitialSettings }) => {
   const { apiKey, ...mapSettings } = mapInitialSettings;
 
-  return (
-    <Wrapper apiKey={apiKey}>
-      <Map mapSettings={mapSettings} />
-    </Wrapper>
-  );
+  const render = (status) => {
+    switch (status) {
+      case Status.LOADING:
+        return <p>...Loading</p>;
+      case Status.FAILURE:
+        return <p>Some error occurs</p>;
+      case Status.SUCCESS:
+        return <Map mapSettings={mapSettings} />;
+      default:
+        return <p></p>;
+    }
+  };
+
+  return <Wrapper apiKey={apiKey} render={render} />;
 };
 
 export default MapContainer;
